780288545c695c08de7341554c43c51894d588db
YCN <alhycnyassine@gmail.com>
2026-04-09 16:15:44 +0100

    update

--- client/config.js
+++ client/config.js
@@ -19,5 +19,5 @@ const checkConfig = (server) => {
   return config;
 };
 
-export const selectServer = "production";
+export const selectServer = "local";
 export const config = checkConfig(selectServer);
--- server/index.mjs
+++ server/index.mjs
@@ -53,7 +53,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"],
   })
 );
 app.use(express.json());
